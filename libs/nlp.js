9f47b1d696a5777ef85dc2db053a254e03878b86
airtoxin <airtoxin@icloud.com>
2015-03-14 18:39:09 +0900

    MeCabの辞書を指定できるようになった

--- libs/nlp.js
+++ libs/nlp.js
@@ -1,5 +1,7 @@
 var _ = require( 'lodash' );
+var config = require( 'config' );
 var Mecab = require( 'mecab-async' );
+if ( config.mecab.dict_path ) Mecab.command = 'mecab -d ' + config.mecab.dict_path;
 var mecab = new Mecab();
 
 var NLP = ( function () {
